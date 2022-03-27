8acf0d3504dd0a58013e42d72ef2485070f98780
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2022-03-27 18:02:36 +0530

    Re-use ApprovalCard and CommentDetail Componenets

--- components/src/index.js
+++ components/src/index.js
@@ -7,13 +7,30 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        avatar={faker.image.avatar()}
-        date="Today At 5:55 AM"
-        description="Lorem Ipsum"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          avatar={faker.image.avatar()}
+          date="Today At 5:55 AM"
+          description="Lorem Ipsum"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          avatar={faker.image.avatar()}
+          date="Today At 5:55 AM"
+          description="Lorem Ipsum"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          avatar={faker.image.avatar()}
+          date="Today At 5:55 AM"
+          description="Lorem Ipsum"
+        />
+      </ApprovalCard>
     </div>
   );
 };
