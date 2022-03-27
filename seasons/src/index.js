0f26916b85696fd58821c79924f3cba6b2862bf3
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2022-03-27 19:26:09 +0530

    Converted App Component to Class Based componenet

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>Hi There!</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Hi There!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
