7ef7e1c9c508eceab438cc761b886d8448a207f2
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2022-05-10 11:58:48 +0530

    Added state update in constructor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,14 +5,15 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => console.log(err)
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-    return <div>Lattitude: {this.state.lat}</div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
