825d90784fd36be5c5109bc51361600354588487
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2022-05-13 13:56:42 +0530

    Updated Season componenet for getting the readable season

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,6 +1,15 @@
 import React from "react";
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winter";
+  } else {
+    return lat > 0 ? "Summer" : "Winter";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
   return <div>Season Display!</div>;
 };
 
