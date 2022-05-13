5de95351f5c685a145a1bd68e2ac4cb66c2e53a0
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2022-05-13 15:35:08 +0530

    Removed conditional rendering from render method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -23,6 +23,10 @@ class App extends React.Component {
     }
 
     return <Loader message="Please accept the location request" />;
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
