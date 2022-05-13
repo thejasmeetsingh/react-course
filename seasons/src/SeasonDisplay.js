57fc4df32298cdb9f6770bed89460065b4e3e5b1
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2022-05-13 14:22:52 +0530

    Added customization and styling for Season Display

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,16 +1,36 @@
+import "./SeasonDisplay.css";
 import React from "react";
+
+const SeasonConfig = {
+  summer: {
+    text: "Burr, It is Chilly",
+    iconName: "snowflake",
+  },
+  winter: {
+    text: "Let's hit the beach",
+    iconName: "sun",
+  },
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? "Summer" : "Winter";
+    return lat > 0 ? "summer" : "winter";
   } else {
-    return lat > 0 ? "Summer" : "Winter";
+    return lat > 0 ? "summer" : "winter";
   }
 };
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  return <div>Season Display!</div>;
+  const { text, iconName } = SeasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
