4bb8e8b5da4e0fda1cceba6999d26901cef5ba5e
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2022-05-21 15:01:31 +0530

    Completed ImageCard Componenet and Added custom grid layout for each image

--- pictures/src/components/ImageCard.js
+++ pictures/src/components/ImageCard.js
@@ -1,11 +1,31 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.ImageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.ImageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.ImageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
+
   render() {
     const image = this.props.image;
     return (
-      <div>
-        <img src={image.urls.regular} alt={image.description} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img
+          ref={this.ImageRef}
+          src={image.urls.regular}
+          alt={image.description}
+        />
       </div>
     );
   }
